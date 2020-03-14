7635cbe93c91d4a75bd7e7562a155816e5e1c737
anamanna <anamanna38@gmail.com>
2020-03-14 14:53:43 +0700

    template

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,7 +13,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('layout.admin');
+        return view('layoutadmin.admin');
     }
 
     /**
--- resources/views/layoutdmin/admin.blade.php
+++ resources/views/layoutdmin/admin.blade.php
@@ -3,7 +3,9 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 3 | Dashboard</title>
+  @section('title')
+
+  @show
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
